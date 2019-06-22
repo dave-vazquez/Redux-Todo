75db33b5df1574682fb1f1298c2b726ff0ce1840
dmvazli <davidmvazquez1@gmail.com>
2019-06-22 18:07:39 -0400

    added data persistance

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle"
 import reducer from "./reducers";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(reducer);
+const persistedState = loadState();
+const store = createStore(reducer, persistedState);
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+}), 1000);
 
 ReactDOM.render(
   <Provider store={store}>
